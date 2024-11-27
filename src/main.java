921c1a3407de1f816edb4a7537feb869e4a44025
Sanjay-1045 <sanjaysukumar10@gmail.com>
2024-11-27 21:11:03 +0530

    First_commit_version2

--- src/main.java
+++ src/main.java
@@ -1,6 +1,8 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import java.time.LocalDateTime;
+import java.util.Scanner;
+
 
 class Ship {
     int length;
@@ -15,6 +17,12 @@ class Ship {
         this.name = name;
         this.arrival = arrival;
         this.departure = departure;
+
+
+    }
+
+    public Ship() {
+
     }
 }
 
@@ -22,9 +30,11 @@ class Ship {
     {int rows;
         int cols;
         int isOccupied;
+        String shipName;
         int len;
         int brd;
         LocalDateTime occupiedUntil;
+
         public Berth(int rows,int cols,int len,int brd)
         {this.rows=rows;
             this.cols=cols;
@@ -33,9 +43,17 @@ class Ship {
             this.brd=brd;
             this.occupiedUntil=null;}
 
-           public int canAccommodate(Ship ship)
-            {
+            public int canAccommodate(Ship ship)
+            {boolean isDateOld=false;
+                if(occupiedUntil!=null && occupiedUntil.isBefore(LocalDateTime.now()))
+                {
+                    isDateOld=true;
+                }
+                if(isOccupied==1 && isDateOld)
+                    isOccupied=0;
+
             return (ship.length <= len && ship.breadth <= brd && isOccupied == 0) ? 1 : 0;
+
         }
     }
         class Layout
@@ -54,13 +72,15 @@ class Ship {
                     }
 
                 }}
-
+            public static String padRight(String s, int n) {
+                return String.format("%-" + n + "s", s);}
                 public void showBerths()
                 {
                     System.out.println("Current Shipyard Layout:");
                     for (int i = 0; i < rows; i++) {
                         for (int j = 0; j < cols; j++) {
-                            System.out.print(berths[i][j].isOccupied==1 ? "1 " : "0 ");
+                            String berth=berths[i][j].isOccupied==1 ? berths[i][j].shipName : "Not_Occuppied";
+                            System.out.print(padRight(berth,20)+"|");
                         }
                         System.out.println();
                     }
@@ -95,6 +115,7 @@ class Ship {
                     if (berthAvailable!=null) {
                         berthAvailable.isOccupied = 1;
                         berthAvailable.occupiedUntil = ship.departure;
+                        berthAvailable.shipName=ship.name;
                         System.out.printf("Ship %s assigned to Berth (%d, %d)", ship.name, berthAvailable.rows, berthAvailable.cols);
                     } else {
                         System.out.printf("No available berth for Ship %s.%n", ship.name);
@@ -109,12 +130,42 @@ class Ship {
                 // to see how IntelliJ IDEA suggests fixing it.
                 Layout layout= new Layout(2,3,150,30);
 
-                Ship ship1 = new Ship(100,15,"SVL", LocalDateTime.parse("2024-10-27T00:00"), LocalDateTime.parse("2024-11-18T00:00"));
-                Ship ship2 = new Ship(130,25,"ASW", LocalDateTime.parse("2024-11-07T00:00"), LocalDateTime.parse("2024-12-28T00:00"));
+                Scanner scanner = new Scanner(System.in);
+                int k=0;
+                while(true) {
+
+                    Ship ship=new Ship();
+                    System.out.printf("Enter length of Ship %d: ", k + 1);
+                    int length = scanner.nextInt();
+                    ship.length=length;
+
+                    System.out.printf("Enter breadth of Ship %d: ", k + 1);
+                   int breadth = scanner.nextInt();
+                    scanner.nextLine();
+                    ship.breadth=breadth;
+
+                    System.out.printf("Enter name of Ship %d: ", k + 1);
+                    String name = scanner.nextLine();
+                    ship.name=name;
+
+                    System.out.printf("Enter arrival date of Ship %d (format: yyyy-MM-dd HH:mm): ", k + 1);
+                    String arrivalInput = scanner.nextLine();
+                    LocalDateTime arrival = LocalDateTime.parse(arrivalInput);
+                    ship.arrival=arrival;
+
+                    System.out.printf("Enter departure date of Ship %d (format: yyyy-MM-dd HH:mm): ", k + 1);
+                    String departureInput = scanner.nextLine();
+                    LocalDateTime departure = LocalDateTime.parse(departureInput);
+                    ship.departure=departure;
+
+                    layout.assignBerth(ship);
+                    System.out.printf("Do you want to continue?Y/N");
+                    k++;
+                    String response=scanner.next();
+                    if(response.equalsIgnoreCase("N"))
+                    break;
+                }
 
                 layout.showBerths();
-                layout.assignBerth(ship1); // Should assign a berth
-                layout.showBerths();
-                layout.assignBerth(ship2); // Should check availability
-                layout.showBerths();
+
             }}
